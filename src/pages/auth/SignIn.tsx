de4f9626f7cb4c7f52a674221620a070c6a28769
feriteja <feriteja@gmail.com>
2022-07-11 08:40:23 +0700

    fixing ui

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,28 +1,35 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const signIn = (e: SyntheticEvent) => {
     e.preventDefault();
   };
   return (
     <div className="flex flex-col justify-center items-center bg-slate-700 h-screen">
-      <div className="w-3/4 bg-slate-50  py-5 px-4 rounded-md shadow-md">
+      <div className="w-3/4 max-w-3xl bg-slate-50  py-5 px-4 rounded-md shadow-md">
         <h1 className="text-xl font-semibold">Login</h1>
         <form onSubmit={signIn} className="flex flex-col mt-5 space-y-3">
           <div className="flex flex-col w-full">
             <label htmlFor="username">Username:</label>
             <input
+              id="username"
               type="text"
               placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
               className="border rounded-md  p-2  "
             />
           </div>
           <div className="flex flex-col w-full">
             <label htmlFor="password">Password:</label>
             <input
+              id="password"
               type="text"
               placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
               className="border rounded-md  p-2  "
             />
           </div>
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,18 +1,25 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPass, setConfPass] = useState("");
+  const [error, setError] = useState("");
+
   const signup = (e: SyntheticEvent) => {
     e.preventDefault();
   };
   return (
     <div className="flex flex-col justify-center items-center bg-slate-700 h-screen">
-      <div className="w-3/4 bg-slate-50  py-5 px-4 rounded-md shadow-md">
-        <h1 className="text-xl font-semibold">Login</h1>
+      <div className="w-3/4 max-w-3xl bg-slate-50  py-5 px-4 rounded-md shadow-md">
+        <h1 className="text-xl font-semibold">Register</h1>
         <form onSubmit={signup} className="flex flex-col mt-5 space-y-3">
           <div className="flex flex-col w-full">
             <label htmlFor="username">Username:</label>
             <input
+              id="username"
+              onChange={(e) => setUsername(e.target.value)}
               type="text"
               placeholder="username"
               className="border rounded-md  p-2  "
@@ -21,6 +28,18 @@ const SignUp = () => {
           <div className="flex flex-col w-full">
             <label htmlFor="password">Password:</label>
             <input
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              type="text"
+              placeholder="password"
+              className="border rounded-md  p-2  "
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="confPassword">Confirm Password:</label>
+            <input
+              id="confPassword"
+              onChange={(e) => setPassword(e.target.value)}
               type="text"
               placeholder="password"
               className="border rounded-md  p-2  "
@@ -36,7 +55,7 @@ const SignUp = () => {
 
       <p className="text-white/80 text-center mt-3 ">
         already have account ?
-        <Link to={"/signup"}>
+        <Link to={"/signin"}>
           <span className="text-indigo-300 cursor-pointer">login</span>
         </Link>
       </p>
